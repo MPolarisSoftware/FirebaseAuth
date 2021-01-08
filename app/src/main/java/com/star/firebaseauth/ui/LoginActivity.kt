560c813ee491720e05c23edc8e2aedfb920a4e17
PolarisSoftDev <53799370+PolarisSoftDev@users.noreply.github.com>
2021-01-08 12:02:18 +0200

    login screen

---
+++ app/src/main/java/com/star/firebaseauth/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.star.firebaseauth.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.star.firebaseauth.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
